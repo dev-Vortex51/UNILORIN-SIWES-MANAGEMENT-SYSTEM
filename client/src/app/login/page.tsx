47c6b84372b0cd6927cbe4a3442df86aaa9bc831
Bello Qudus Folohunsho <qudusbello51@gmail.com>
2026-07-19 20:40:01 +0100

    chore: reduce the space between the logo and the login card

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -75,7 +75,7 @@ export default function LoginPage() {
         <Image
           src="/icons/app-icon.svg"
           alt="ITMS Logo"
-          h={300}
+          h={200}
           fit="contain"
           mx="auto"
           // mb={8}
